a51f93b63a5f43dfb955adcf279f1543e51fb9b7
ff <nandofuentes01@gmail.com>
2023-05-15 11:35:59 -0600

    Changes may 15 2023 -change text field for a content field in basic-page template

--- site/templates/basic-page.php
+++ site/templates/basic-page.php
@@ -1,5 +1,39 @@
 <div id="main-content" pw-append>
     <div class="uk-container uk-container-small uk-margin uk-margin-top">
-        <?=$page->text?>
+        <?php foreach ($page->content as $item): ?>
+
+            <?php if ($item->type == "text_modulo"): ?>
+                <div class="">
+                    <div class="">
+                        <?= $item->text; ?>
+                    </div>
+                </div>
+                <hr class="">
+            <?php endif ?>
+
+
+            <?php if ($item->type == "galeria_modulo"): ?>
+                <div class="uk-margin-large-top uk-width-1-1@m">
+                    <div uk-slideshow>
+                        <ul class="uk-slideshow-items">
+                            <?php foreach ($item->galeria as $image): ?>
+                                <li>
+                                    <img src="<?= $image->url ?>" alt="<?= $image->description ?>"
+                                         loading="lazy" uk-cover>
+                                    <div class="uk-overlay uk-overlay-primary uk-position-bottom-center uk-text-center">
+                                        <p class="uk-margin-remove"><?= $image->description ?></p>
+                                    </div>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                        <a class=" uk-position-small" href="#" uk-slidenav-previous
+                           uk-slideshow-item="previous"></a>
+                        <a class=" uk-position-small" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+                    </div>
+                </div>
+                <hr class="">
+            <?php endif ?>
+        <?php endforeach; ?>
+
     </div>
 </div>
